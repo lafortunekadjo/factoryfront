c06258def06fb604beee32007e1c0a825e008cc7
lafortunekadjo <lafortunekadjo@outlook.fr>
2026-07-14 20:01:57 +0100

    mise a jour prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'  // Même origine en prod (nginx proxy)
+  apiUrl: 'http://72.62.28.14:8081/api'  // Même origine en prod (nginx proxy)
 };
