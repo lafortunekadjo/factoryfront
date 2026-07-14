355b548156255bc7bdea469ac447d319b87a3701
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2026-07-14 22:51:51 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://72.62.28.14:8081/api'  // Même origine en prod (nginx proxy)
+  apiUrl: 'http://my2-0.cloud:8081/api'  // Même origine en prod (nginx proxy)
 };
